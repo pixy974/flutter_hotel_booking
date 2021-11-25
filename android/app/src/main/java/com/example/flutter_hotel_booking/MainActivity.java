e307c3d8b59b350313850bccc9dfcf39ddf7dadd
TURPIN Claire <c.turpin@ymail.com>
2021-11-25 14:55:16 +0100

    initilaisation du projet

---
+++ android/app/src/main/java/com/example/flutter_hotel_booking/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_hotel_booking;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'Hotels Booking',
+      home: Container(),
+    );
+  }}
